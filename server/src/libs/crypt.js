a34d6ca9c1c0e5ab480a22fe18840cff87a3198a
Ilya <rickmortyand4@gmail.com>
2020-08-04 18:38:55 +0300

    Completed chat services and added crypt, formatDate

--- server/src/libs/crypt.js
+++ server/src/libs/crypt.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const CryptoJS = require('crypto-js');
 
 const hash = str => {
     const salt = bcrypt.genSaltSync(10);
@@ -6,11 +7,17 @@ const hash = str => {
     return { hash: hash, salt: salt};
 };
 
+const token = () => {
+    const key = CryptoJS.enc.Hex.stringify(CryptoJS.lib.WordArray.random(16));
+    return key;
+}
+
 const compare = (s, hash) => {
     return bcrypt.compareSync(s, hash);
 };
 
 module.exports = {
     hash: hash,
-    compare: compare
+    compare: compare,
+    token: token
 };
