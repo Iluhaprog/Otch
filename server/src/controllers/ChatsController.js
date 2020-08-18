0e4f1d24c25ab74b37c7395f7350c1999f52cab7
Ilya <rickmortyand4@gmail.com>
2020-08-18 15:59:10 +0300

    Add Chats controller

---
+++ server/src/controllers/ChatsController.js
@@ -0,0 +1,43 @@
+const ChatsService = require('../services/ChatService');
+
+class ChatsController {
+
+    async getById(req, res) {
+        const id = req.body.id;
+        const result = await ChatsService.getById(id);
+        res.json(result);
+    }
+
+    async getByKey(req, res) {
+        const key = req.body.key;
+        const result = await ChatsService.getByKey(key);
+        res.json(result);
+    }
+
+    async getByUserId(req, res) {
+        const user_id = req.body.user_id;
+        const result = await ChatsService.getByUserId(user_id);
+        res.json(result);
+    }
+
+    async create(req, res) {
+        const chat = req.body.chat;
+        const result = await ChatsService.create(chat);
+        res.json(result);
+    }
+
+    async addMember(req, res) {
+        const { member_id, key } = req.body;
+        const result = await ChatsService.addMember(member_id, key);
+        res.json(result);
+    }
+
+    async deleteById(req, res) {
+        const id = req.body.id;
+        const result = await ChatsService.deleteById(id);
+        res.json(result);
+    }
+
+}
+
+module.exports = new ChatsController();
