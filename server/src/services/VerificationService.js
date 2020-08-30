86adade8fe4d258f279456e981237b74ecdf8bcb
Ilya <rickmortyand4@gmail.com>
2020-08-30 19:05:18 +0300

    Added verification code service

---
+++ server/src/services/VerificationService.js
@@ -0,0 +1,92 @@
+const query = require('../libs/connection');
+const Answer = require('../libs/Answer');
+const token = require('../libs/crypt');
+const { SUCCESS, FAILURE, RECORD_NF } = require('../libs/statuses');
+
+class VerificationService {
+
+    /**
+     * Compare two codes
+     * 
+     * @param {number} code1 
+     * @param {number} code2
+     * @returns {boolean} If code1 equals code2, then true, otherwise false 
+     */
+    async compareCodes(code1, code2) {
+        return code1 === code2;
+    }
+
+    /**
+     * Return verification code by id
+     * 
+     * @param {number} id
+     * @returns {Answer} If verification code exist, then Answer contains status SUCCESS and data is verification code, otherwise status FAILURE and data is empty
+     */
+    async getById(id) {
+        const [[verificationCode]] = await query('SELECT * FROM VerificationCodes WHERE `id`=?', [id]);
+        return verificationCode 
+                    ? new Answer(SUCCESS, verificationCode) 
+                    : new Answer(RECORD_NF);
+    }
+
+    /**
+     * Return verification code by user id
+     * 
+     * @param {number} userId
+     * @returns {Answer} If verification code exist, then Answer contains status SUCCESS and data is verification code, otherwise status FAILURE and data is empty
+     */
+    async getByUserId(userId) {
+        const [[verificationCode]] = await query('SELECT * FROM VerificationCodes WHERE `user_id`=?', [userId]);
+        return verificationCode 
+                    ? new Answer(SUCCESS, verificationCode) 
+                    : new Answer(RECORD_NF);
+    }
+
+    /**
+     * Create verification code
+     * 
+     * @param {number} userId 
+     * @returns {Answer} If verification code created then Answer contains status SUCCESS and data is code, otherwise status FAILURE
+     */
+    async create(userId) {
+        if (userId) {
+            const code = token().slice(0, 6);
+            await query('INSERT INTO VerificationCodes(`code`, `user_id`) VALUE (?, ?)', [code, userId]);
+            return new Answer(SUCCESS, {code: code});
+        }
+        return new Answer(FAILURE);
+    }
+
+    /**
+     * Update verification code
+     * 
+     * @param {number} id
+     * @returns {Answer} If verification code updated, then Answer contains status SUCCESS, otherwise status FAILURE 
+     */
+    async update(id) {
+        const result = await this.getById(id);
+        if (result.getStatus() === SUCCESS) {
+            const newCode = token().slice(0, 6);
+            await query('UPDATE VerificationCodes SET `code`=? WHERE `id`=?', [newCode, id]);
+            return new Answer(SUCCESS);
+        }
+        return new Answer(FAILURE);
+
+    }
+
+    /**
+     * Delete verification code
+     * 
+     * @param {number} id 
+     * @returns {Answer} If verification code deleted, then Answer contains status SUCCESS, otherwise status FAILURE 
+     */
+    async deleteById(id) {
+        if (id) {
+            await query('DELETE FORM VerificationCodes WHERE `id`=?', [id]);
+            return new Answer(SUCCESS);
+        }
+        return new Answer(FAILURE);
+    }
+}
+
+module.exports = new VerificationService();
