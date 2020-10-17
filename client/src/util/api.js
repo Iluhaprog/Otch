591c1607f8d7ffa77f0ffb68ffa1df92fdfd5613
Ilya <rickmortyand4@gmail.com>
2020-10-17 19:38:29 +0300

    Add functions for work with rest api

---
+++ client/src/util/api.js
@@ -0,0 +1,20 @@
+import { apiUrl } from './../config';
+
+const toParams = obj => Object.keys(obj).map(([key, value]) => `${key}=${value}`).join('&');
+
+const request = method => {
+    return ({url, body, params, headers}) => {
+        return  fetch(`${apiUrl}${url}?${toParams(params)}`,{
+                    method: method,
+                    headers: headers,
+                    body: JSON.stringify(body),
+                });
+    }
+}
+
+const GET = request('GET');
+const POST = request('POST');
+const PUT = request('PUT');
+const DELETE = request('DELETE');
+
+export { GET, POST, PUT, DELETE };
