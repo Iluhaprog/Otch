3d8645464e33432c60c19a606a30a94e815c0809
Ilya <rickmortyand4@gmail.com>
2020-10-20 19:43:52 +0300

    Add testing app structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import { Verification } from './components/Verification/Verification';
-import { SignIn } from './components/SignIn/SignIn';
-import { SignUp} from './components/SignUp/SignUp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.scss';
+
+import Auth from './components/auth/auth';
+import Main from './components/main/main';
+
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isAuthenticate: false,
-            userId: -1,
+            userId: getCookie('userId') || -1,
         }
+    }
+
+    changeAuthStatus(userId, isAuthenticate) {
+        document.cookie = `userId=${userId}`;
+        this.setState({
+            userId: userId,
+            isAuthenticate: isAuthenticate,
+        });
     }
 
     render() {
         return (
-            <div className='app'>
-                <SignIn />
-                <hr />
-                <SignUp />
-                <hr />
-                <Verification />
-            </div>
+            <Router>
+                <div className='app'>
+                    <Route path='/auth'>
+                        <Auth changeAuthStatus={(userId, isAuthenticate) => this.changeAuthStatus(userId, isAuthenticate)} />
+                    </Route>
+                    <Route exec path='/'>
+                        <Main isAuthenticate={this.state.isAuthenticate} userId={this.state.userId} />
+                    </Route>
+                </div>
+            </Router>
         );
     }
 }
