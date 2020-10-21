973bccf8adb3d843f8e5860193cdaba80f95a365
Ilya <rickmortyand4@gmail.com>
2020-10-21 21:08:16 +0300

    add component with user info

---
+++ client/src/components/main/userInfo/userInfo.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { setCookie } from '../../../util/cookie';
+import { getById } from './../../../api/user.api';
+import emptyUser from '../../../assets/images/emptyUser.png';
+import './userInfo.scss';
+
+class UserInfo extends React.Component {
+    state = {
+        userName: '',
+        image: '',
+        failure: false,
+    }
+
+    componentDidMount() {
+        getById(this.props.userId)
+            .then(result => {
+                this.setState({
+                    userName: result.data.name,
+                    image: result.data.avatar_image,
+                })
+            })
+            .catch(err => {
+                this.setState({failure: true})
+                setCookie({
+                    isAuth: false,
+                    userId: -1,
+                });
+                console.error(err);
+            });
+    }
+
+    render() {
+        if (this.state.failure) return <Redirect to='/auth' />;
+        const userAvatar = this.state.image;
+
+        return (
+            <div className="user-info">
+                <div className="row row_ai-c">
+                    <div>
+                        <div className="avatar row_jc-c row_ai-c">
+                            <img src={userAvatar || emptyUser} alt="emptyUser" />
+                        </div>
+                    </div>
+                    <h1 className="user-name">
+                        {this.state.userName}
+                    </h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserInfo;
