63dec6d03b5d9784967c36ad51327cad8b2a7603
Ilya Novak <rickmortyand4@gmail.com>
2020-11-15 17:06:59 +0300

    Little fixea

--- client/src/components/main/Messenger/MessagesList/MessagesListItem/MessagesListItem.js
+++ client/src/components/main/Messenger/MessagesList/MessagesListItem/MessagesListItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { apiUrl } from '../../../../../config';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
 
+import { apiUrl } from '../../../../../config';
 import { getFileType } from '../../../../../util/file';
- 
+
 export default props => {
     const right = parseInt(props.memberId) === parseInt(props.userId) ? '' : 'r';
     const fileType = getFileType(props.filePath);
@@ -14,22 +16,28 @@ export default props => {
                         {props.senderName}
                     </p>
                 </div>
-                <div className="message-text">
-                    <p>
-                        {props.message}
-                    </p>
-                </div>
-                {props.filePath 
-                        ? 
-                        <div className="message-text file">
-                            {
-                                fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png' ?
-                                <img src={`${apiUrl}/${props.filePath}`} alt='file'/> :
-                                ''
-                            }
+                {props.message 
+                    ?
+                        <div className="message-text">
+                            <p>
+                                {props.message}
+                            </p>
                         </div>
-                        : ''
-                    }
+                    : ''
+                }
+                {props.filePath
+                    ?
+                    <div className="message-text file">
+                        {
+                            fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png' ?
+                                <img src={`${apiUrl}/${props.filePath}`} alt='file' /> :
+                                <a href={`${apiUrl}/${props.filePath}`} download>
+                                    File download <FontAwesomeIcon icon={faFileDownload} />
+                                </a>
+                        }
+                    </div>
+                    : ''
+                }
             </div>
         </li>
     );
