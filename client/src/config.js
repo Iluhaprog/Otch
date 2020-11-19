b92a1d35dceb7153d6841dc001c6834faab622f4
Ilya Novak <rickmortyand4@gmail.com>
2020-11-19 22:03:41 +0300

    change api urls

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-const apiUrl = 'https://localhost:3000';
-const apiWsHost = 'wss://localhost:3000';
+const apiUrl = 'http://localhost:3000';
+const apiWsHost = 'ws://localhost:3000';
 const openWeatherKey = '3aed9e318281351e1f3e1c7d89211784';
 
 export { 
