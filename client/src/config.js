e68270ac818903eec1f8e71a37d8b87fa229b9fd
Ilya Novak <rickmortyand4@gmail.com>
2020-12-02 00:48:04 +0300

    Add api of production

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,8 @@
-const apiUrl = 'http://192.168.100.5:3000';
-// const apiUrl = 'http://localhost:3000';
-//const apiWsHost = 'ws://localhost:3000';
-const apiWsHost = 'ws://192.168.100.5:3000';
+const apiUrl = 'https://otch-server.herokuapp.com';
+const apiWsHost = 'wss://otch-server.herokuapp.com';
 
 const openWeatherKey = '3aed9e318281351e1f3e1c7d89211784';
- 
+  
 export { 
     apiUrl,
     openWeatherKey,
